70c96c37e6201068daf2b2f63e2a2376d9365c5b
John Doe <johndoe@example.com>
2024-04-01 15:05:16 -0400

    Included User Api

--- src/auth/Auth0ProviderNavigate.tsx
+++ src/auth/Auth0ProviderNavigate.tsx
@@ -1,4 +1,5 @@
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
     children: React.ReactNode;
@@ -6,22 +7,27 @@ type Props = {
 }
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+    const navigate = useNavigate();
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-    if (!domain || !clientId || !redirectUri) {
+    if (!domain || !clientId || !redirectUri || !audience ) {
         throw new Error ("unable to initialize auth");
     }
 
-    const onRedirectCallback = (appState?: AppState, user?: User) => {
-        console.log("USER", user);
-    }
+    const onRedirectCallback = () => {
+        // console.log("USER", user);
+        navigate("/auth-callback");
+    };
+
     return (
         <Auth0Provider domain={domain}
         clientId= {clientId}
         authorizationParams={{
             redirect_uri: redirectUri,
+            audience,
         }}
         onRedirectCallback={onRedirectCallback}
         >
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,24 @@ import '../src/global.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './AppRoutes'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderNavigate';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+const queryClient = new QueryClient ({ 
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
+      <QueryClientProvider client={queryClient}>
       <Auth0ProviderWithNavigate>
            <AppRoutes /> 
       </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
 )
