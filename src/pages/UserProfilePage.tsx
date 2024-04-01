6a99bdc86e75125906d2ce37508f94d2f6a7eb1a
John Doe <johndoe@example.com>
2024-04-01 17:14:38 -0400

    Updated form functionality

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,7 +1,15 @@
+import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
-    return <UserProfileForm />;
-}
+    const { currentUser, isLoading: isGetLoading } = useGetMyUser();
+    const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
+
+    if (isGetLoading) {
+        return <span> Loading...</span>;
+    }
+    
+    return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} />;
+};
  
 export default UserProfilePage;
